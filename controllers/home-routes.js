38a5936d63b23b548e553cbcb0cfb5d908ba9378
Dong Nguyen <dnguye621@gmail.com>
2022-10-28 11:10:17 -0500

    added leaderboard

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,5 +30,28 @@ router.get('/login', (req, res) => {
     res.render('login', { userPage : true });
   });
   
+router.get('/leaderboard', withAuth, async (req, res) => {
+  try {
+    const userScore = await User.findByPk(req.session.user_id);
+    res.render('menu', { menuPage : true, userScore : userScore.get({plain : true}) })
+
+  } catch(err) {
+    res.status(500).json(err)
+  }
+  res.render('leaderboard', { leaderboardPage : true });
+});
+  
+
+
+
+router.get('/how-to-play', withAuth, (req, res) => {
+  res.render('how-to-play', { h2pPage : true })
+})
+
+
+router.get('/profile', withAuth, (req, res) => {
+  res.render('profile', { profilePage : true });
+});
+
 
 module.exports = router;
